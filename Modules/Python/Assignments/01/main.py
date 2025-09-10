42918c179bf53f45a3271409307ff4faffdf7b60
Mahros AL-Qabasy <mahros.elqabasy@hotmail.com>
2025-09-10 12:56:20 -0400

    Save Changes

--- Modules/Python/Assignments/01/main.py
+++ Modules/Python/Assignments/01/main.py
@@ -1,5 +1,7 @@
 #!/bin/python
 
+import sys
+
 
 from modules.program import Program
 from modules.program_one import ProgramOne
@@ -8,26 +10,40 @@ from modules.program_three import ProgramThree
 from modules.program_four import ProgramFour
 from modules.program_seven import ProgramSeven
 
+
+# programs
 programs:list[Program] = [
-    # ProgramOne,
-    # ProgramTwo,
-    # ProgramThree,
-    # ProgramFour,
+    ProgramOne,
+    ProgramTwo,
+    ProgramThree,
+    ProgramFour,
     ProgramSeven,
 ]
 
 
 
 # main
-def main():
-    print("[A] Assignment Solution Started!\n")
-
-    
-    for index in range(0, len(programs)):
-        ins = programs[index](index+1)
+def main(pid:int=None):
+    if pid == None:
+        print("[A] Assignment Solution Started!\n")
+        for index in range(0, len(programs)):
+            ins = programs[index](id=index+1)
+            ins.run()
+            ins.end()
+    else:
+        ins = programs[pid-1](id=pid)
         ins.run()
         ins.end()
 
+
+
 # run
 if __name__ == "__main__":
-    main()
+    try:
+        if len(sys.argv) < 2:
+            main()
+        else:
+            main(pid=int(sys.argv[1]))
+    except Exception as Error:
+        print(Error)
+        print("[!] Unexpected Error Happened.")
